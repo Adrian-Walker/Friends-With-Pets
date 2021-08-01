4dc57d621fb476f442b84fe30ed1ae9cb7e3e8f4
Adrian Walker <awalker3365@gmail.com>
2021-08-01 13:26:10 -0400

    8-1-21 130p

--- src/Friend.js
+++ src/Friend.js
@@ -1,7 +1,14 @@
 import React from "react"
 
-function Friend() {
-    return
+function Friend(props) {
+    return (
+        <div>
+            <h2>Name:{props.name}</h2>
+            <h2>Age: {props.age}</h2>
+            <h2>Pet: {props.pets.name}</h2>
+
+        </div>
+    )
 }
 
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './style.css';
 
 
-import App from './App';
+import App from './FriendList';
 
 
 
