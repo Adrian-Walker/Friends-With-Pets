17ef4c8f2f42fed32874b5c432f13e10f643f160
Adrian Walker <awalker3365@gmail.com>
2021-08-01 23:43:00 -0400

    8-1-21 1142p

--- src/Friend.js
+++ src/Friend.js
@@ -3,7 +3,7 @@ import React from "react"
 function Friend(props) {
     return (
         <div>
-            <h2>Name:{props.name}</h2>
+            <h2>Name: {props.name}</h2>
             <h2>Age: {props.age}</h2>
             <h2>Pet: {props.pets.name}</h2>
 
